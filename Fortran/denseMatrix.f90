d275152838af1ca2c007498dc0f97ea6b47101a5
Jacobi-Leo <liuzeyu271828@gmail.com>
2017-05-12 22:43:53 +0800

    All feature except solving added, only several not finished.

--- Fortran/denseMatrix.f90
+++ Fortran/denseMatrix.f90
@@ -3,13 +3,14 @@
 ! This module contains the definition of matrix and some basic operations,
 ! still under development.
 !
-! This code must be compiled with (in gfortran) -fdefault-real-8 and -std=f2003,
+! This code must be compiled with -fdefault-real-8 (in gfortran)
 ! or -autodouble (in ifort).
 !
 ! Since Fortran 2003 features are massively utilized in this code,
 ! gfortran version>=5.0 is required (v6.0 or higher is recommended),
-! and ifort 17 is verified while lower versions not tested.
-
+! and ifort 17 is verified while lower versions not tested (according to
+! official document, ifort 16 is the first edition of ifort with full
+! Fortran 2003 support).
 
 module denseMatrix
   use constant
@@ -19,15 +20,30 @@ module denseMatrix
   !======================================================================
   ! Error Information List
   !   info = 127    The previous operation has unmatched dimension
+  !   info = 126    Trying to use unallocated components
+  !======================================================================
+  ! Error information is used for debugging and testing whether the
+  ! previous operation is valid, never judge the feasibility of one
+  ! operation with attribute /info/
   !======================================================================
   type, public :: Matrix
      private
      integer :: nrow = 0  ! number of rows
      integer :: ncol = 0  ! number of columns
+     integer :: nupper = 0  ! number of superdiagonals
+     integer :: nlower = 0  ! number of subdiagonals
      real(kind=WP), allocatable :: comp(:,:)  ! components of the matrix
+     real(kind=WP), allocatable :: band(:,:)  ! form of band matrix
      integer :: info = 0  ! error information
      logical :: diagonal = .false.
      logical :: bidiagonal = .false.
+     logical :: tridiagonal = .false.
+     logical :: hessenberg = .false. ! upper Hessenberg considered only
+     logical :: banded = .false.     ! not implemented
+     logical :: symmetric = .false.
+     logical :: hermitian = .false.  ! not implemented
+     logical :: orthogonal = .false. ! not implemented
+     logical :: positiveDefinite = .false. ! not implemented
    contains
      !!===== Assignment and operators, as well as related. =======
      generic, public :: assignment(=) => arrayToMatrix, dArrayToMatrix
@@ -46,20 +62,75 @@ module denseMatrix
      procedure, private, pass :: matrixTimesint
      procedure, private, pass :: matrixTimesMatrix
      !!===========================================================
-     procedure, public, pass :: T  ! transpose
-     procedure, public, pass :: writeToFile
      procedure, public, pass :: isAllocated
      procedure, public, pass :: isDiagonal
      procedure, public, pass :: isBidiagonal
+     procedure, public, pass :: isTridiagonal
+     procedure, public, pass :: isHessenberg
+     procedure, public, pass :: isSymmetric
+     procedure, public, pass :: setDiagonal
+     procedure, public, pass :: setBidiagonal
+     procedure, public, pass :: setTridiagonal
+     procedure, public, pass :: setHessenberg
+     procedure, public, pass :: setSymmetric
      procedure, public, pass :: getNrow
      procedure, public, pass :: getNcolumn
      procedure, public, pass :: getEllement
+     procedure, public, pass :: printSpecialAttributes   ! not finished
+     procedure, public, pass :: resetToGeneral           ! not finished
+     !!===========================================================
+     procedure, public, pass :: T  ! transpose
+     procedure, public, pass :: writeToFile
      ! procedure, pass :: inv
      ! procedure, pass :: solve
      final :: matrixClean
   end type matrix
 
 contains
+
+  subroutine printSpecialAttributes ( self )
+    class(Matrix), intent(in) :: self
+  end subroutine printSpecialAttributes
+
+  subroutine resetToGeneral ( self )
+    class(Matrix), intent(inout) :: self
+  end subroutine resetToGeneral
+
+  subroutine setHessenberg ( self, status )
+    class(Matrix), intent(inout) :: self
+    logical, intent(in) :: status
+    self%hessenberg = status
+  end subroutine setHessenberg
+
+  subroutine setTridiagonal ( self, status )
+    class(Matrix), intent(inout) :: self
+    logical, intent(in) :: status
+    self%tridiagonal = status
+  end subroutine setTridiagonal
+
+  subroutine setDiagonal ( self, status )
+    class(Matrix), intent(inout) :: self
+    logical, intent(in) :: status
+    self%diagonal = status
+  end subroutine setDiagonal
+
+  subroutine setBidiagonal ( self, status )
+    class(Matrix), intent(inout) :: self
+    logical, intent(in) :: status
+    self%bidiagonal = status
+  end subroutine setBidiagonal
+
+  subroutine setSymmetric ( self, status )
+    class(Matrix), intent(inout) :: self
+    logical, intent(in) :: status
+    self%bidiagonal = status
+  end subroutine setSymmetric
+
+  function isSymmetric ( self ) result ( r )
+    class(Matrix), intent(in) :: self
+    logical :: r
+    r = self%symmetric
+  end function isSymmetric
 
   function isDiagonal ( self ) result ( r )
     class(Matrix), intent(in) :: self
@@ -73,11 +144,27 @@ contains
     r = self%bidiagonal
   end function isBidiagonal
 
+  function isTridiagonal ( self ) result ( r )
+    class(Matrix), intent(in) :: self
+    logical :: r
+    r = self%tridiagonal
+  end function isTridiagonal
+
+  function isHessenberg ( self ) result ( r )
+    class(Matrix), intent(in) :: self
+    logical :: r
+    r = self%hessenberg
+  end function isHessenberg
+
   function getEllement ( self, i, j ) result ( r )
     class(Matrix), intent(in) :: self
     real(kind=WP) :: r
     integer, intent(in) :: i, j
-    r = self%comp(i,j)
+    if ( self%isAllocated() ) then
+       r = self%comp(i,j)
+    else
+       stop '126'
+    end if
   end function getEllement
 
   function getNrow ( self ) result ( r )
@@ -102,6 +189,11 @@ contains
     class(Matrix), intent(out) :: mat
     real(kind=WP), intent(in), dimension(:,:) :: array
     integer :: m, n
+
+    if ( mat%isAllocated() ) then
+       deallocate( mat%comp )
+    end if
+
     m = size(array, 1)
     n = size(array, 2)
     allocate( mat%comp( m, n ) )
@@ -115,6 +207,11 @@ contains
     class(Matrix), intent(out) :: mat
     real(kind=WP), intent(in), dimension(:) :: array
     integer :: m
+
+    if ( mat%isAllocated() ) then
+       deallocate( mat%comp )
+    end if
+
     m = size(array)
     allocate( mat%comp( m, 1 ) )
     mat%comp(:,1) = array
@@ -122,6 +219,44 @@ contains
     mat%ncol = 1
     mat%info = 0
   end subroutine dArrayToMatrix
+
+  function matrixAdd ( self, mat ) result ( r )
+    !==========================================
+    ! If the dimension of self and mat does not
+    ! match, program stopped.
+    ! If the parameter self is not allocated, mat
+    ! is returned with error info.
+    ! If the parameter mat is not allocated, self
+    ! is returned with error info.
+    !
+    ! The following three procedures are based on
+    ! this one.
+    !==========================================
+    type(Matrix) :: r
+    class(Matrix), intent(in) :: self, mat
+    integer :: m1, m2, n1, n2
+
+    m1 = self%nrow
+    n1 = self%ncol
+    m2 = mat%nrow
+    n2 = mat%ncol
+
+    if ( .not. self%isAllocated() ) then
+       r = mat
+       r%info = 126
+    else if ( .not. mat%isAllocated() ) then
+       r = self
+       r%info = 126
+    else if ( m1 /= m2 .or. n1 /= n2 ) then
+       r%info = 127
+       stop '127'
+    else
+       r%nrow = m1
+       r%ncol = n1
+       allocate( r%comp(m1,n1) )
+       r%comp = self%comp + mat%comp
+    end if
+  end function matrixAdd
 
   function arrayAdd ( self, array )
     type(Matrix) :: arrayAdd
@@ -133,40 +268,17 @@ contains
     arrayAdd = self + here
   end function arrayAdd
 
-  function matrixAdd ( self, mat )
-    type(Matrix) :: matrixAdd
-    class(Matrix), intent(in) :: self, mat
-    integer :: m1, m2, n1, n2
-
-    m1 = self%nrow
-    n1 = self%ncol
-    m2 = mat%nrow
-    n2 = mat%ncol
-
-    if ( m1 /= m2 .or. n1 /= n2 ) then
-       matrixAdd%nrow = -1
-       matrixAdd%ncol = -1
-       matrixAdd%info = 127
-    else
-       matrixAdd%nrow = m1
-       matrixAdd%ncol = n1
-       allocate( matrixAdd%comp(m1,n1) )
-       matrixAdd%comp = self%comp + mat%comp
-    end if
-  end function matrixAdd
-
   subroutine addMatrix ( self, mat )
     class(Matrix), intent(in) :: mat
     class(Matrix), intent(inout) :: self
     integer :: m1, m2, n1, n2
 
-    m1 = self%nrow
-    n1 = self%ncol
-    m2 = mat%nrow
-    n2 = mat%ncol
-
-    if ( m1 /= m2 .or. n1 /= n2 ) then
+    if ( .not. self%isAllocated() ) then
+       self%info = 126
+       return
+    else if ( self%nrow /= mat%nrow .or. self%ncol /= mat%ncol ) then
        self%info = 127
+       return
     else
        self%comp = self%comp + mat%comp
     end if
@@ -180,16 +292,26 @@ contains
     matrixSubtract = self + neg * mat
   end function matrixSubtract
 
-  function realTimesMatrix ( a, self )
-    type(Matrix) :: realTimesMatrix
+  function realTimesMatrix ( a, self ) result ( r )
+    !=================================================
+    ! This function is the basic scalar multiplication
+    ! function and all other three ones just call this
+    ! function.
+    !=================================================
+    type(Matrix) :: r
     real(kind=WP), intent(in) :: a
     class(Matrix), intent(in) :: self
 
-    realTimesMatrix%nrow = self%nrow
-    realTimesMatrix%ncol = self%ncol
+    if ( .not. self%isAllocated() ) then
+       r%info = 126
+       return
+    end if
 
-    if ( allocated(self%comp) ) then
-       realTimesMatrix%comp = a * self%comp
+    r%nrow = self%nrow
+    r%ncol = self%ncol
+
+    if ( self%isAllocated() ) then
+       r%comp = a * self%comp
     end if
   end function realTimesMatrix
 
@@ -201,8 +323,8 @@ contains
     matrixTimesReal%nrow = self%nrow
     matrixTimesReal%ncol = self%ncol
 
-    if ( allocated(self%comp) ) then
-       matrixTimesReal%comp = a * self%comp
+    if ( self%isAllocated() ) then
+       matrixTimesReal = a * self
     end if
   end function matrixTimesReal
 
@@ -230,8 +352,12 @@ contains
     type(Matrix) :: matrixTimesMatrix
     class(Matrix), intent(in) :: self, mat
 
-    if ( self%ncol /= mat%nrow .or. allocated( self%comp ) == .false. .or. allocated( mat%comp ) == .false. ) then
+    if ( self%ncol /= mat%nrow ) then
        matrixTimesMatrix%info = 127
+       return
+    else if ( ( .not. self%isAllocated() ) .or. ( .not. mat%isAllocated() ) ) then
+       matrixTimesMatrix%info = 126
+       return
     else
        matrixTimesMatrix%nrow = self%nrow
        matrixTimesMatrix%ncol = mat%ncol
@@ -243,13 +369,14 @@ contains
     class(Matrix), intent(inout) :: self
     integer :: tmp
 
-    if ( allocated(self%comp) ) then
+    if ( ( .not. self%isAllocated() ) .or. self%isSymmetric() ) then
+       return
+    else
        tmp = self%nrow
        self%nrow = self%ncol
        self%ncol = tmp
        self%comp = transpose(self%comp)
     end if
-
   end subroutine T
 
   subroutine writeToFile ( self, fileUnit )
@@ -257,29 +384,32 @@ contains
     integer, intent(in) :: fileUnit
     integer :: i, j
 
-    !! This is a bad solution
-    !!========================================================
-    ! do i = 1, self%nrow
-    !    do j = 1, self%ncol
-    !       write(fileUnit, *) self%comp(i,j)
-    !    end do
-    !    write(fileUnit,*)
-    ! end do
-
-    ! exactly the way I want
-    do i = 1, self%nrow
-       write(fileUnit, *) ( self%comp(i,j), j = 1, self%ncol )
-    end do
+    if ( .not. self%isAllocated() ) then
+       return
+    else
+       !! This is a bad solution
+       !!==========================================!
+       ! do i = 1, self%nrow                       !
+       !    do j = 1, self%ncol                    !
+       !       write(fileUnit, *) self%comp(i,j)   !
+       !    end do                                 !
+       !    write(fileUnit,*)                      !
+       ! end do                                    !
+       !===========================================!
+       !! exactly the way I want
+       do i = 1, self%nrow
+          write(fileUnit, *) ( self%comp(i,j), j = 1, self%ncol )
+       end do
+    end if
 
   end subroutine writeToFile
 
   subroutine matrixClean ( self )
     type(Matrix), intent(inout) :: self
-
     !! This is debug info
     !write(*,*) "In finalizer"
 
-    if ( allocated( self%comp ) ) then
+    if ( self%isAllocated() ) then
        deallocate( self%comp )
     end if
   end subroutine matrixClean
