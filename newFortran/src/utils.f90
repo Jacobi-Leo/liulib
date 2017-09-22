2c031294595aa7745783b3a90b8822780cca4b09
Jacobi-Leo <liuzeyu271828@gmail.com>
2017-09-22 20:48:05 +0800

    utils.f90 updated.

--- newFortran/src/utils.f90
+++ newFortran/src/utils.f90
@@ -7,15 +7,16 @@ contains
 
   function outputName ( unit ) result ( name )
     integer, intent(in) :: unit
-    character(len=15) :: name
+    character(len=32) :: name
     character(len=10) :: cTemp
 
     if ( unit > 999999999 ) then
        stop 'unit too large!'
     end if
 
-    write(cTemp, '(i10)') unit
+    write(cTemp, '(i16)') unit
     name = 'data' // trim(adjustl(cTemp)) // '.out'
   end function outputName
+
 end module utils
 
