a8aac7a96d358fe1aad1ec5e46cd1e925a86b9ee
Jacobi-Leo <liuzeyu271828@gmail.com>
2017-09-26 15:49:50 +0800

    Added linearAlgebra module file.

---
+++ newFortran/src/linearAlgebra.f90
@@ -0,0 +1,43 @@
+module linearAlgebra
+  use constant
+  use denseMatrix
+  implicit none
+  public
+
+contains
+  function linspace ( a1, an, n) result (l)
+    integer, intent(in) :: n
+    real(kind=WP), dimension(n) :: l
+    real(kind=WP), intent(in) :: a1, an
+
+    real(kind=WP) :: step
+    integer :: i
+
+    if ( a1 < an .or. n < 2 ) then
+       stop 'Invalid argument'
+    end if
+
+    step = (an - a1) / (n-1)
+
+    do i = 1, n
+       l(i) = a1 + (i-1) * step
+    end do
+  end function linspace
+
+  function logspace ( a1, an, n, base ) result (l)
+    integer, intent(in) :: n
+    real(kind=WP), intent(in) :: a1, an
+    real(kind=WP), intent(in), optional :: base
+    real(kind=WP), dimension(n) :: l, ll
+
+    if ( .not. present( base ) ) then
+       base = 10.0_WP
+    end if
+
+    ll = linspace(a1, an, n)
+    l = base**ll
+  end function logspace
+
+end module linearAlgebra
+
+
